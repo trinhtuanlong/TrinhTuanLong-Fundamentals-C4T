a1b4ac3116382693303596ed35cc82d4fbf72d0d
Trinh Tuan Long <trinhtuanlong2k@gmail.com>
2018-05-04 05:49:49 +0700

    first commit

--- Session3/Exercise9.py
+++ Session3/Exercise9.py
@@ -1,20 +1,42 @@
-class Food():
-  def __init__(self): None
-class Customer():
-  def __init__(self):
-    self.name = input("What is your name?")
-    self.order = list(input("Give me:").split(","))
-class Employer():
-  def __init__(self,order): None
-  def response(self):
-    print("Your lunch is being prepared.")
-class Lunch():
-  def __init__(self): None
-  def f(self,order): return print("Your lunch is here.")
-A = Customer()
-B = Employer(A.order)
-B.response()
-C = Lunch()
-C.f(A.order)
+class Employee:
+    order = []
+
+    def takeOrder(self, foodName):
+        print("Anything else?")
+        self.order.append(foodName)  # return a Food, with requested name
 
 
+class Customer:
+    def __init__(self):
+        self.food = []  # initialize my food to None
+
+    def placeOrder(self, foodName, employee):
+        self.food.append(foodName)  # place order with an Employee
+        return self.food
+    def printFood(self):
+        print(self.food)  # print the name of my food
+
+
+class Food:
+    def __init__(self, name): None  # store food name
+
+
+class Lunch:
+    def __init__(self):
+        self.cus = Customer()
+        self.emp = Employee()  # make/embed Customer and Employee
+
+    def order(self):
+        while True:
+            a = input("I want to order")
+            if a == " Nothing else": break  # start a Customer order simulation
+            else: self.emp.takeOrder(self.cus.placeOrder(a,self.emp))
+        return self.emp.order
+
+    def result(self):
+        print("Your meal consists of:", self.emp.order)  # ask the Customer what kind of Food it has
+
+
+a = Lunch()
+a.order()
+a.result()
